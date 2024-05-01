a7562564d0bdd5a139542c7fa1089ebabb289e47
Marcos Paulo <marcos.paulodasilva.mp@gmail.com>
2024-05-01 15:01:46 -0300

    feat: added option to see all songs

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import { handler } from "./frontend/build/handler.js";
 import fs from "fs";
 import path from "path";
 import cors from "cors";
+import NodeCache from "node-cache";
 
 const PORT = 3000;
 const app = express();
 
 app.use(cors());
+const cache = new NodeCache();
 
 const s3 = `https://s3.marcospaulo.dev.br`
 
@@ -16,25 +18,33 @@ const defaultPath = path.join(process.cwd(), "music")
 const normalizeName = (item) => item.replaceAll(" ", "-").toLowerCase();
 
 app.get("/api/albums/:album", (req, res) => {
+  const currentPath = req.path;
+  let data = cache.get(currentPath);
+
   const { album } = req.params;
   const albumPath = path.join(defaultPath, album);
-  const cover = `${s3}/${normalizeName(album)}/${normalizeName(album)}.jpg`
+  const cover = `${s3}/${normalizeName(album)}/${normalizeName(album)}.jpg`;
 
-  const albumSongs =  fs.readdirSync(albumPath).filter(file => file.endsWith('.mp3')).map((song) => {
+  if (!data) {
+    const albumSongs = fs.readdirSync(albumPath).filter(file => file.endsWith('.mp3')).map((song) => {
       const songName = song.replace(".mp3", "");
-      const cover = `${s3}/${normalizeName(album)}/${normalizeName(songName)}.jpg`
+      const cover = `${s3}/${normalizeName(album)}/${normalizeName(songName)}.jpg`;
 
       return {
         name: songName,
         cover
-      }
-  });
+      };
+    });
 
-  return res.json({
-    name: album,
-    cover,
-    songs: albumSongs 
-  });
+    data = {
+      name: album,
+      cover,
+      songs: albumSongs
+    };
+    cache.set(currentPath, data, 3600);
+  }
+
+  return res.json(data);
 });
 
 app.get("/api/albums/:album/:song", (req, res) => {
@@ -66,6 +76,43 @@ app.get("/api/albums", (_req, res) => {
 
   return res.json(albums);
 });
+
+app.get("/api/songs", (req, res) => {
+  const currentPath = req.path;
+  let data = cache.get(currentPath);
+
+  const songs = [];
+
+  if (!data) {
+    fs.readdirSync(defaultPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .forEach(dirent => {
+        const albumPath = path.join(defaultPath, dirent.name);
+
+        const currentSongs = fs.readdirSync(albumPath).filter(file => file.endsWith('.mp3'));
+
+        const defaultCover = `${s3}/${normalizeName(dirent.name)}/${normalizeName(dirent.name)}.jpg`
+
+        for (const song of currentSongs) {
+          const songName = song.replace(".mp3", "");
+
+          const cover = `${s3}/${normalizeName(dirent.name)}/${normalizeName(songName)}.jpg`
+
+          songs.push({
+            name: songName,
+            cover,
+            defaultCover,
+            album: dirent.name
+          });
+        }
+      });
+
+    data = songs;
+    cache.set(currentPath, data, 3600);
+  }
+
+  return res.json(data);
+})
 
 app.use(handler);
 
